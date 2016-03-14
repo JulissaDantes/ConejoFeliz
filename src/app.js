2600c07427a1b99bae97f38bef744c8ed23a9e90
JulissaDantes <julissadcj@gmail.com>
2016-03-13 21:18:26 -0430

    Update app.js
    Con el  movimiento del conejo

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 
+
 var HelloWorldLayer = cc.Layer.extend({
     sprFondo:null,
     sprConejo:null,
@@ -16,17 +17,87 @@ var HelloWorldLayer = cc.Layer.extend({
         this.sprConejo = new cc.Sprite(res.conejo_png);
         this.sprConejo.setPosition(size.width / 2,size.height * 0.15);
         this.addChild(this.sprConejo, 1);
-
+        
+       
 
         return true;
     }
 });
+
+ 
+/*var MouseTest = EventTest.extend({
+    init:function () {
+        this._super();
+        //posicionando la imagen del conejo
+        this.sprConejo = new cc.Sprite(res.conejo_png);
+        this.sprConejo.setPosition(size.width / 2,size.height * 0.15);
+        this.addChild(this.sprConejo);
+        sprConejo.x = 0;
+        sprConejo.y = 0;
+
+       
+        if( 'mouse' in cc.sys.capabilities ) 
+        {
+            cc.eventManager.addListener({
+                event: cc.EventListener.MOUSE,
+                
+                onMouseMove: function(event){
+                    var pos = event.getLocation(), target = event.getCurrentTarget();
+                    cc.log("onMouseMove at: " + pos.x + " " + pos.y );
+                    target.sprConejo.x = pos.x;
+                    target.sprConejo.y = pos.y;
+                    }
+                },
+                 this);
+        } else {
+            cc.log("MOUSE Not supported");
+        }
+    }
+});*///
 
 var HelloWorldScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
         var layer = new HelloWorldLayer();
         this.addChild(layer);
+    }
+});
+//MOVER EL CONEJO
+var MouseTest = HelloWorldLayer.extend({
+    init:function () {
+        this._super();
+
+        if( 'mouse' in cc.sys.capabilities ) {
+            cc.eventManager.addListener({
+                event: cc.EventListener.MOUSE,
+                onMouseDown: function(event){
+                    var pos = event.getLocation(), target = event.getCurrentTarget();
+                    if(event.getButton() === cc.EventMouse.BUTTON_RIGHT)
+                        cc.log("onRightMouseDown at: " + pos.x + " " + pos.y );
+                    else if(event.getButton() === cc.EventMouse.BUTTON_LEFT)
+                        cc.log("onLeftMouseDown at: " + pos.x + " " + pos.y );
+                    target.this.sprConejo.x = pos.x;
+                    target.this.sprConejo.y = pos.y;
+                },
+                onMouseMove: function(event){
+                    var pos = event.getLocation(), target = event.getCurrentTarget();
+                    cc.log("onMouseMove at: " + pos.x + " " + pos.y );
+                    target.this.sprConejo.x = pos.x;
+                    target.this.sprConejo.y = pos.y;
+                },
+                onMouseUp: function(event){
+                    var pos = event.getLocation(), target = event.getCurrentTarget();
+                    target.this.sprConejo.x = pos.x;
+                    target.this.sprConejo.y = pos.y;
+                    cc.log("onMouseUp at: " + pos.x + " " + pos.y );
+                }
+            }, this);
+        } else {
+            cc.log("MOUSE Not supported");
+        }
+    },
+    subtitle:function () {
+        return "Mouse test. Move mouse and see console";
     }
 });
 
